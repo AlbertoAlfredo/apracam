2ea2f4d6a9145c85eff449f07c2d953c684a57e4
Alberto Alfredo <raioalfredo@gmail.com>
2022-05-04 23:24:12 -0300

    teste importações css

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,16 @@
-<script setup>
-import TheWelcome from '@/components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <h1>Teste</h1>
+    <p>Este aqui é o primeiro teste para verificar se as importações do css funcionaram</p>
   </main>
 </template>
+<style scoped>
+  h1{
+    font-family: altone;
+    font-weight: bold;
+    font-style: oblique;
+  }
+  p{
+    font-family: altone;
+  }
+</style>
