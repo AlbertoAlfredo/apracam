709dc0d5a2e11fcb605c5bb4d18f9b947281b1e1
AlbertoAlfredo <raioalfredo@gmail.com>
2022-05-07 02:16:09 -0300

    Mais um pouquinho

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -23,7 +23,45 @@
             font-weight: bold;
           " >Torna-se<br/>um Associado</div>
         </RouterLink>
+        <div>
+          <div>
+            <img style="width: 4vw;" src="@/assets/img/ICONES/ATENDENTE.png" alt="Icone de atendente">
+            <p>Quer saber mais?<br/>Fale com um consultor</p>
+          </div>
+          <div>
+            <img style="width: 4vw;" src="@/assets/img/ICONES/FEEDBACK.png" alt="Icone de feedback">
+            <p>Nossos Feedback<br/>O que estão falando</p>
+          </div>
+        </div>
       </div>
+    </section>
+    <section>
+
+        <h2>QUEM SOMOS</h2>
+        <p>Somos a ASSOCIAÇÃO PARANAENSE DE APOIO A CANNABIS MEDICINAL (APRACAM), fundada em 10 de dezembro de 2019 e registrada em 19 de fevereiro de 2020 no município de Umuarama, Paraná, Brasil, inscrita no CNPJ: 36.716.085/0001-56. Surgimos pela demanda emergente local de prover os benefícios medicinais da Cannabis para a população que necessita. </p>
+
+    </section>
+    <section>
+      <div>
+        <img style="width: 4vw;" src="@/assets/img/ICONES/FEEDBACK.png" alt="Icone de feedback">
+        <h2>MISSÃO</h2>
+        <p>Agregar melhoria na qualidade de vida das pessoas, informar sobre o direito ao auto cultivo,..</p>
+      </div>
+      <div>
+        <img style="width: 4vw;" src="@/assets/img/ICONES/FEEDBACK.png" alt="Icone de feedback">
+        <h2>VISÃO</h2>
+        <p>Tornar-se referência em Cannabis medicinal no Brasil. Abranger todas as vertentes que a Cannabis está envolvida. Contemplar uma produção de Cannabis medicinal </p>
+        <div>Saber mais</div>
+      </div>
+      <div>
+        <img style="width: 4vw;" src="@/assets/img/ICONES/FEEDBACK.png" alt="Icone de feedback">
+        <h2>VALORES</h2>
+        <p>Trabalho sério, honestidade, justiça, respeito à natureza, cumprir com a constituição nacional, sustentab... </p>
+      </div>
+    </section>
+    <section>
+      <p>Pode se associar, Todo cidadão de bem, pessoa física ou jurídica que compreendam a importância e os benefícios dos derivados do <span>Cannabis</span></p>
+      <p>O associado pode se enquadrar em três categorias de membros associados: MEMBROS BENEMÉRITOS, MEMBROS CONTRIBUINTES, MEMBROS SOCIAIS</p>
     </section>
   </main>
 </template>
